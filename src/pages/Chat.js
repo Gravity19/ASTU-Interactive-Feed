ed479934c0198265ced472b29fed3cc65fba9c53
Gravity19 <65826968+Gravity19@users.noreply.github.com>
2023-06-02 16:16:10 +0300

    minor updates to chat and others

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -113,7 +113,7 @@ function Chat() {
 
     // Active Click Chat
 
-    const [activeChat, setActiveChat] = useState(null);
+    const [activeChat, setActiveChat] = useState('0');
     const [activeChatID, setActiveChatID] = useState(null);
 
     const handleChatClick = (chat) => {
@@ -128,7 +128,7 @@ function Chat() {
             <SideBar />
 
 
-            {/* Discussion Section */}
+            {/* ======= Discussion Section ====== */}
 
             <div class="chat-box">
                 <div class="row">
@@ -142,8 +142,6 @@ function Chat() {
                         </div>
 
                         <div className='scroll-discussions'>
-
-                        {/* Discussion-Box from Database */}
                             
                         {chats.map((chat, index) => (
                         <div key={index}>
@@ -158,7 +156,7 @@ function Chat() {
                                     }}
                                     >
                             
-                                <div className="photo" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80)'}}></div>
+                                <div className="photo" style={{backgroundImage: 'url(https://i.pinimg.com/564x/0f/91/5a/0f915a0565ffb4470a78552eed75ac83.jpg)'}}></div>
                                 <div className="desc-contact">
                                     <p className="name">{chat.topic}</p>
                                     <p className="message">Let's meet for a coffee CSS word-wrap property is used to break the long words.</p>
@@ -169,19 +167,6 @@ function Chat() {
                         </div>
                         ))}
 
-                        {/* Discussion-Box from Database -END */}
-
-                            <div className={`discussion ${activeChat === 5 ? 'message-active' : ''}`} onClick={() => handleChatClick(5)}>
-                            
-                                <div class="photo" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80)'}}>
-                                    <div class="online"></div>
-                                </div>
-                                <div class="desc-contact">
-                                    <p class="name">Campus Group</p>
-                                    <p class="message">Let's meet for a coffee CSS word-wrap property is used to break the long words.</p>
-                                </div>
-                                <div class="timer">12 sec</div>
-                            </div>
 
                         </div>
                         
@@ -189,10 +174,10 @@ function Chat() {
 
 
 
-            {/* CHAT-Box Database */}
+            {/* ====== CHAT SECTION ======= */}
 
 
-                {activeChat === activeChatID ? (
+                {activeChat === activeChatID && activeChat !=='0' ? (
                 <>
                     <section class="chat" style={{ zIndex: zIndex2 }}>
                         <div class="header-chat">
@@ -202,8 +187,33 @@ function Chat() {
 
                         
                         <div class="messages-chat"> 
+
                         {message.map((messages, i) => (
                         <div key={i}>
+
+                            {messages.senderType === 'Student' && messages.userid === name.studentId ? (
+
+                            <div class="message-container-right">
+                                <div class="message text-only">
+                                    <div class="response">
+                                    <p class="text">{messages.message}</p>
+                                    </div>
+                                </div>
+                                {/* <div class="response-time"> 15h04</div> */}
+                            </div>
+
+                            ) : messages.senderType === 'staff' && messages.userid === name.staffId ? (
+
+                            <div class="message-container-right">
+                                <div class="message text-only">
+                                    <div class="response">
+                                    <p class="text">{messages.message}</p>
+                                    </div>
+                                </div>
+                                {/* <div class="response-time"> 15h04</div> */}
+                            </div>
+
+                            ) : (
 
                             <div class="message-container">
                                 <div class="message">
@@ -213,11 +223,15 @@ function Chat() {
                                         <div className='focus-text'>{messages.message}</div>
                                     </div>
                                 </div>
-                                <p class="time"> 15h09</p>
+                                {/* <p class="time"> 15h09</p> */}
                             </div>
+
+                            )}
+                                
+
                         </div>
                         ))}
-                            
+
                         </div>
 
 
@@ -252,61 +266,6 @@ function Chat() {
                 </>
                 )}
 
-
-
-
-
-
-
-            {/* ===== custom chat === */}
-
-            {activeChat === 5 && (
-            <>
-                    <section class="chat">
-                        <div class="header-chat">
-                            <i class="icon fa fa-user-o" aria-hidden="true"></i>
-                            <p class="name">Campus Group</p>
-                            <i class="icon clickable fa fa-ellipsis-h right" aria-hidden="true"></i>
-                        </div>
-
-                        <div class="messages-chat"> 
-
-                            <div class="message-container">
-                                <div class="message">
-                                    <div class="photo" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80)'}}>
-                                    <div class="online"></div>
-                                    </div>
-                                    <div class="text">
-                                        <div className='name-text'>Beth Ruth</div>
-                                        <div className='focus-text'>ADD is the term commonly used to describe symptoms of inattention,distractibility.
-                                            ADHD is the term used to describe  . 9 pm at the bar if possible 😳
-                                        </div>
-                                    </div>
-                                </div>
-                                <p class="time"> 14h58</p>
-                            </div>
-                        
-                            <div class="message-container-right">
-                                <div class="message text-only">
-                                    <div class="response">
-                                    <p class="text"> When can we meet ?</p>
-                                    </div>
-                                </div>
-                                <div class="response-time"> 15h04</div>
-                            </div>
-
-                        </div>
-
-                        <div class="footer-chat">
-                            <input type="text" className="write-message" placeholder="Type your message here"></input>
-                            <button className='send_button'><IoSend className="icon"/></button>
-                        </div>
-
-                    </section>
-                            
-                            
-            </>
-            )}
 
 
                 </div>
